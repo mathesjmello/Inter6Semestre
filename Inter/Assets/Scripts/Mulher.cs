fc34441b413573e16a373c3abab9c59484d12158
Matias <matiascodas@gamil.com>
2019-05-10 12:18:41 -0300

    NavMesh

--- Inter/Assets/Scripts/Mulher.cs
+++ Inter/Assets/Scripts/Mulher.cs
@@ -5,23 +5,46 @@ using UnityEngine;
 public class Mulher : MonoBehaviour
 {
 
-    public Animator anim;
+  /* private Transform target;
+
+    public GameObject point1;
+
+    public GameObject point2;
+
+    public int changeDirection = 1;
+    // Angular speed in radians per sec.
+    float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+            target = point1.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
 
-    void OnTriggerStay( Collider other){
-        if(other.CompareTag("Player")){
-            anim.speed = 1.5f;
-        }
+        
+        //if (changeDirection == 1)
+        //{
+        
+            
+        //}
+
+
+
+        Vector3 targetDir = target.position - transform.position;
+
+        // The step size is equal to speed times frame time.
+        float step = speed * Time.deltaTime;
+
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 1.0f);
+        Debug.DrawRay(transform.position, newDir, Color.red);
+
+
+
     }
+    */
+
 }
