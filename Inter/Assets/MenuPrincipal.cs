f0ce03e86079ba095e6b5206509aff1114388404
César <cronokami@hotmail.com>
2019-05-10 21:48:04 -0300

    Umas atualizadas no menu principal
    ainda incompleto

--- Inter/Assets/MenuPrincipal.cs
+++ Inter/Assets/MenuPrincipal.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuPrincipal : MonoBehaviour
 {
 	public GameObject MainMenuScreen;
 	public GameObject OptionScreen;
+	public GameObject NewGameScreen;
 
+	public void Jogar()
+	{
+		MainMenuScreen.SetActive(!MainMenuScreen.activeSelf);
+		NewGameScreen.SetActive(true);
+
+	}
 	public void Opções()
 	{
 		MainMenuScreen.SetActive(!MainMenuScreen.activeSelf);
@@ -17,8 +25,28 @@ public class MenuPrincipal : MonoBehaviour
 
 	public void VoltaOpções()
 	{
+		NewGameScreen.SetActive(false);
 		OptionScreen.SetActive(false);
 		MainMenuScreen.SetActive(true);
+
+	}
+
+	public void Créditos()
+	{
+		SceneManager.LoadScene(1);
+
+	}
+
+
+	public void QuitGame()
+	{
+
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#endif
+
+		Application.Quit();
+
 
 	}
 }
