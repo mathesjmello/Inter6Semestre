9956b068b5eca4a8768aa08a0e6127bed1d0ad38
César <cronokami@hotmail.com>
2019-05-10 21:27:49 -0300

    Menu Pause
    e partes do principal e opções iniciado

---
+++ Inter/Assets/MenuPrincipal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPrincipal : MonoBehaviour
+{
+	public GameObject MainMenuScreen;
+	public GameObject OptionScreen;
+
+	public void Opções()
+	{
+		MainMenuScreen.SetActive(!MainMenuScreen.activeSelf);
+		OptionScreen.SetActive(true);
+
+
+	}
+
+	public void VoltaOpções()
+	{
+		OptionScreen.SetActive(false);
+		MainMenuScreen.SetActive(true);
+
+	}
+}
