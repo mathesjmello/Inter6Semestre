49da2f8f502d131aa23d2f33bb59e5f1cc894eb3
César <cronokami@hotmail.com>
2019-05-11 00:31:11 -0300

    Menu Principal e Créditos(ficou bem fofo)

---
+++ Inter/Assets/Creditos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creditos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKey(KeyCode.Space)) Time.timeScale += 0.1f;
+		else Time.timeScale = 1;
+        
+    }
+}
