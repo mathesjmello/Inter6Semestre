bd5e353984b638eb3ec22d0fec04c4ced438e235
Matias <matiascodas@gamil.com>
2019-05-13 17:28:09 -0300

    Tirando os Scripts

--- Inter/Assets/Scripts/TesteCharDrunk.cs
+++ Inter/Assets/Scripts/TesteCharDrunk.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class TesteCharDrunk : MonoBehaviour
 {
-	public GameObject player;
-	public bool isWalking = false;
+	private CharacterController controller;
 
+	public GameObject player;
+	public GameObject serguei;
 	public GameObject cam;
 	public GameObject mark;
+	public GameObject canvas;
+	public GameObject dimiCaido;
+	public GameObject dimiCarregado;
+	public GameObject totem;
+	public GameObject camUp;
+	public GameObject PauseScreen;
 
-	private CharacterController controller;
+
+	public bool isWalking = false;
+	public bool vodka;
+	public bool carregando = false;
+	public  bool comCalca = false;
+	public bool isWalkPlaying;
+	public bool isWalkingBack;
+	public bool feedPlay;
+
+
 	private float AngularSpeed = 100;
 	public float walkSpeed ;
 	public float playerSpeed = 1.5f;
@@ -18,76 +34,30 @@ public class TesteCharDrunk : MonoBehaviour
 	public float jumpSpeed = 5;
 	public float mouseSensivity = 30;
 	private float rotationX = 0;
-	private Vector3 moveDirection = Vector3.zero;
-
-	public Animator playerAnim;
-
-	public Animator playerDimiAnim;
-
-	public Animator dimiAnim;
-
-	public GameObject canvas;
-
-
-
-
-
-
-	// Variaveis para as mecanicas de bebado//
 	public float drunkSpeed;
-
 	public float drunkMax = 1.2f;
-
 	public float drunkMin = -1.2f;
-
-	public bool vodka;
-
 	public float minTime = 2;
 	public float maxTime = 5;
-
 	public float time = 0;
-
 	public float changeTime;
 
+	public Animator playerAnim;
+	public Animator playerDimiAnim;
+	public Animator dimiAnim;
 
-	public bool carregando = false;
-
-
-	public GameObject dimiCaido;
-
-	public GameObject dimiCarregado;
+	private Vector3 moveDirection = Vector3.zero;
 
 
-	public GameObject camUp;
-
-
-	public GameObject PauseScreen;
-
-
-	public  bool comCalca = false;
-
-
-	//variaveis som
 	public AudioClip walkSound;
-
 	public AudioSource playerSound;
-
-	public bool isWalkPlaying;
-
-	public bool isWalkingBack;
-
-	public bool respawn;
-
-	public Vector3 respawnPos;
-
-	public GameObject serguei;
-
-	public GameObject totem;
-
-	public bool holdTotem;
+	public AudioSource feedback;
 
 
-	
+
+
+
+
 
 
 
@@ -96,10 +66,6 @@ public class TesteCharDrunk : MonoBehaviour
 		controller = GetComponent<CharacterController>();
 		Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false;
-
-
-		respawnPos = transform.position;
-		
 	}
 
 
@@ -111,17 +77,7 @@ public class TesteCharDrunk : MonoBehaviour
 			totem.transform.parent = null;
 		}
 
-
-		if (respawn == true)
-		{
-				
-		Respawn();
-			
-		}
-
-
-
-		// para dar efeito de andar bebado//	
+	
 		if (carregando == true)
 		{	
 			serguei.SetActive(false);
@@ -145,14 +101,13 @@ public class TesteCharDrunk : MonoBehaviour
 
 
 
-		// para levantar o bebado//
-
 		if (carregando == false)
 		{
 			serguei.SetActive(true);
 			dimiCaido.SetActive(true);
 		    dimiCarregado.SetActive(false);
 			drunkSpeed = 0;
+
 		}
 
 
@@ -218,6 +173,7 @@ public class TesteCharDrunk : MonoBehaviour
 
 		}
 
+		
 		moveDirection.y -= gravity;
 
 		controller.Move(moveDirection * Time.deltaTime);
@@ -305,9 +261,8 @@ public class TesteCharDrunk : MonoBehaviour
 		rotationX += Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime * -1;
 		rotationX = Mathf.Clamp(rotationX, -45, 45);
 
-		cam.transform.localEulerAngles = new Vector3(-rotationX,
-													 cam.transform.localEulerAngles.y,
-													 cam.transform.localEulerAngles.z);
+		cam.transform.localEulerAngles = new Vector3(-rotationX,cam.transform.localEulerAngles.y,cam.transform.localEulerAngles.z);
+
 	}
 
 
@@ -323,59 +278,9 @@ public class TesteCharDrunk : MonoBehaviour
 		 
 	}
 
-	void OnTriggerEnter(Collider other)
-	{
-
-		/*if (other.CompareTag("Limites"))
-		{
-			movel.GetComponent<Movel>().ArrastaMov = false;
-		}*/
-
-	}
 
 
-	void OnTriggerStay(Collider other)
-	{
-
-		if (other.CompareTag("Segurança") && Input.GetKey(KeyCode.E) && vodka == true)
-		{
-			other.GetComponent<FieldOfView>().drunk = true;
-			vodka = false;
-		}
-
-		if (other.CompareTag("Vodka"))
-		{
-			vodka =true;
-		}
-
-		if (other.CompareTag("InsideArm"))
-		{
-			cam.SetActive(false);
-			camUp.SetActive(true);
-		}
-	}
-
-	 void OnTriggerExit(Collider other) 
-	 {
-		 
-		if (other.CompareTag("InsideArm"))
-		{
-			cam.SetActive(true);
-			camUp.SetActive(false);
-		}
-		
-	}
-
-
-
-
-	void Respawn(){
-
-		transform.position = respawnPos;
-
-
-	}
-	}
+}
 }
 
 
