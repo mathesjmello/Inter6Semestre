03e9d79bf942a969e181b2888ba129d360b82f63
César <cronokami@hotmail.com>
2019-05-14 13:42:27 -0300

    Começando tutorial
    e tentando arrumar as fontes

---
+++ Inter/Assets/Tutorial.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+
+	public bool primeiroTut;
+	public GameObject canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+		primeiroTut = true; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player" && primeiroTut)
+		{
+			canvas.SetActive(true);
+
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Player" && primeiroTut)
+		{
+			canvas.SetActive(false);
+
+		}
+	}
+}
