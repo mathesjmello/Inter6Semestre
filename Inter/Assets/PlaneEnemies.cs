094d752c3d15b365ea8533d3aeae87d031dae09f
Cronokami <cronokami@hotmail.com>
2019-05-21 20:44:01 -0300

    Adicionando inimigo no joguinho do avião e explosão dele

---
+++ Inter/Assets/PlaneEnemies.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneEnemies : MonoBehaviour
+{
+	public Transform focalPoint;
+	private Vector3 direction;
+	public float speed = .75f;
+
+	void Start()
+	{
+		
+	
+		direction = focalPoint.transform.position - transform.position;  
+		direction.Normalize();
+	}
+
+	void Update()
+	{
+		// Move in direction of target.
+		float deltaSpeed = speed * Time.deltaTime;
+		transform.Translate(direction.x * deltaSpeed, direction.y * deltaSpeed, direction.z * deltaSpeed, Space.World);
+	}
+}
