6be66fc488fe26cb802b2cf11441fe8bf5f73f8d
Matheus <matheus.mello@vetorzero.com.br>
2019-05-27 16:06:09 -0300

    Loading e Cut Scene ok

---
+++ Inter/Assets/Scripts/LoadSistem/CutSceneManeger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class CutSceneManeger : MonoBehaviour
+{
+    public VideoPlayer VideoClip;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        VideoClip.loopPointReached += EndReached;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene("LoadingScene");
+    }
+    void EndReached(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene("LoadingScene");
+    }
+}
---
+++ Inter/Assets/Scripts/LoadSistem/LoadScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadScreen : MonoBehaviour {
+
+    AsyncOperation operaçao;
+
+    public Slider barraDeLoading;
+
+    // Use this for initialization
+    void Start () {
+
+        StartCoroutine(nameof(LevelLoad));
+        
+    }
+
+    IEnumerator LevelLoad()
+    {
+        operaçao = SceneManager.LoadSceneAsync("Fase01");
+
+        while (!operaçao.isDone)
+        {
+            float processo = Mathf.Clamp01(operaçao.progress / .5f);
+            barraDeLoading.value = 1 - processo;
+            yield return null;
+        }
+    }
+}
