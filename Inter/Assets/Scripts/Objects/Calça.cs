996b336ab0de063200df7f36885de5cab08cb2cb
Khalil Yamada Gangeme <khalil.yamada@hotmail.com>
2019-05-28 23:42:16 -0300

    Tutorial reposicionado // Câmera ativa com fala da mulher (calça)
    Tutoriais reposicionados para que o player possa ver do novo ponto de início
    A câmera de quando o player pega a calça e mostra a mulher agora foram juntadas (para serem ativadas as hitbox ao mesmo tempo)

--- Inter/Assets/Scripts/Objects/Calça.cs
+++ Inter/Assets/Scripts/Objects/Calça.cs
@@ -8,6 +8,8 @@ public class Calça : MonoBehaviour
     public GameObject fala_04;
     public GameObject fala_05;
     public GameObject fala_06;
+
+    public GameObject cameraMulher;
     
     public GameObject dimitri;
     public GameObject esposaFala;
@@ -43,17 +45,19 @@ public class Calça : MonoBehaviour
         fala_04.SetActive(false);
         fala_05.SetActive(false);
         fala_06.SetActive(false);
+        cameraMulher.SetActive(false);
     }
 
 
-    public void Update(){
+    public void Update()
+    {
         if (saveGame)
         {
             PegouCalca();
         }
     }
 
-     private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
@@ -65,22 +69,23 @@ public class Calça : MonoBehaviour
 
     public void PegouCalca()
     {
-            mesaSalao.SetActive(false);
-            carrinhos.SetActive(false);
-            esposaFala.SetActive(false);
-            song.SetActive(true);
-			//Destroy(this.gameObject);
-            calca.SetActive(false);
-			portaGrandeAnim.SetBool("Aberto", true);
-            fala_04.SetActive(true);
-            fala_05.SetActive(true);
-            fala_06.SetActive(true);
-            bomba.SetActive(true);
-            dimitri.GetComponent<LargaDimi>().comCalca = true;
-			tutoriais.SetActive(false);
-			tutoriais2.SetActive(true);
-            check.fase = 1;
-            checkPoints.SetActive(true);
-            canto.Stop();
+        cameraMulher.SetActive(true);
+        mesaSalao.SetActive(false);
+        carrinhos.SetActive(false);
+        esposaFala.SetActive(false);
+        song.SetActive(true);
+        //Destroy(this.gameObject);
+        calca.SetActive(false);
+        portaGrandeAnim.SetBool("Aberto", true);
+        fala_04.SetActive(true);
+        fala_05.SetActive(true);
+        fala_06.SetActive(true);
+        bomba.SetActive(true);
+        dimitri.GetComponent<LargaDimi>().comCalca = true;
+        tutoriais.SetActive(false);
+        tutoriais2.SetActive(true);
+        check.fase = 1;
+        checkPoints.SetActive(true);
+        canto.Stop();
     }
 }
