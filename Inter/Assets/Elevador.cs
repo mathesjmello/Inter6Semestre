4b4111f08c8a273a7c6e0360813e5f0a371360c0
Matias <matiascodas@gamil.com>
2019-05-29 08:37:55 -0300

    Entrada para o bunker

---
+++ Inter/Assets/Elevador.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Elevador : MonoBehaviour
+{
+
+    public Respawn acabou;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other){
+        if(other.CompareTag("Player") && acabou.fase >= 4){
+            SceneManager.LoadScene("Fase02");
+        }
+    }
+}
