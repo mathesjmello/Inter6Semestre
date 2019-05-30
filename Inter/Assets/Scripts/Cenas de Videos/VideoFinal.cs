1e401d150b18d30ce8dce6f73c7d03d891debc7b
Matias <matiascodas@gamil.com>
2019-05-30 01:16:38 -0300

    Arrumando os scripts

---
+++ Inter/Assets/Scripts/Cenas de Videos/VideoFinal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+
+
+
+
+public class VideoFinal : MonoBehaviour
+{
+
+
+    public VideoPlayer VideoClip;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        VideoClip.loopPointReached += EndReached;
+    }
+
+        public void NextScene()
+    {
+        SceneManager.LoadScene("Fase01");
+    }
+    void EndReached(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene("Fase01");
+    }
+}
