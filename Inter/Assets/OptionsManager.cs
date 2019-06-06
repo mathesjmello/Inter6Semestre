36e4c1339e19d3e0ba4ea0eb17822c339866f379
Cronokami <cronokami@hotmail.com>
2019-06-05 21:59:42 -0300

    Brilho funciona(na fase02)

---
+++ Inter/Assets/OptionsManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.UI;
+
+public class OptionsManager : MonoBehaviour
+{
+    public PostProcessProfile PostProcessProfile;
+    private ColorGrading colorGrading;
+    private MasterVolume MasterVolume;
+    private TesteCharDrunk TesteCharDrunk;
+
+    public GameObject brilhoEAudioObject;
+    public GameObject sensibilObject;
+
+    static private float brilho = 0;
+    static private float sensibilidade = 30;
+    static private float volume = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MasterVolume = brilhoEAudioObject.GetComponent<MasterVolume>();
+        TesteCharDrunk = sensibilObject.GetComponent<TesteCharDrunk>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PostProcessProfile.TryGetSettings(out colorGrading);
+        colorGrading.brightness.value = brilho;
+        print(colorGrading.postExposure.value);
+    }
+
+
+    public void Brilho(float valor)
+    {
+        brilho = valor;
+       
+    }
+
+    public void Sensibilidade(float valor)
+    {
+        sensibilidade = valor;
+    }
+
+    public void Volume(float valor)
+    {
+        volume = valor;
+    }
+}
